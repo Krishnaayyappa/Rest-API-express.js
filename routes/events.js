696f5730cb51fe46eb1e785091b1d6623aff7150
Hari Krishna Ayyappa Narina <krishnanarina8@gmail.com>
2023-11-22 16:34:22 -0500

    Solved events bug

--- routes/events.js
+++ routes/events.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.get('/events', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     console.log("123")
     const events = await getAll();
